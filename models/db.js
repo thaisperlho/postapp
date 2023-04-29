884696fbfefb4b0a8e9e74024a328196267e88d8
Thais Pereira de Carvalho <56940730+thaisperlho@users.noreply.github.com>
2023-04-28 21:23:04 -0300

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,9 +1,9 @@
 const Sequelize = require('sequelize');
 //Conexãocomobancodedados
-const sequelize = new Sequelize('epiz_34100740_postapp','epiz_34100740','1234@',{
-    host:"sql304.epizy.com",
-    port:"3306",
-    dialect:'mysql'
+const sequelize = new Sequelize('wqnxlawm','wqnxlawm','otnl9cbXm68uLvpDmaPKGJpTiQAxMF6T',{
+    host:"motty.db.elephantsql.com",
+    port:"5432",
+    dialect:'postgres'
 });
 //Vamosexportarasvariáveis
 module.exports = {
